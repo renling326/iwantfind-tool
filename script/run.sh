dfaca00a6505403acc34d35971c9416230e7e1db
RenBaoling <renbobo326@gmail.com>
2016-11-20 17:40:49 +0800

    run script

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 spark-submit  --class com.iwantfind.Analyser  /Users/renbaoling/Documents/workspace/iwantfind-tool/target/iwantfind-tool-0.1-SNAPSHOT.jar   /Users/renbaoling/Documents/workspace/iwantfind-tool/target/
+
+
+ spark-submit  --class com.iwantfind.ASAnalyser  /Users/renbaoling/Documents/workspace/iwantfind-tool/target/iwantfind-tool-0.1-SNAPSHOT.jar   /Users/renbaoling/Desktop/data_for_test1120/data_for_test/as_log
